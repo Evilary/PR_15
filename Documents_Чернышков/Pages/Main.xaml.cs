b5167b9a2a1aa285be606ea3eea596f066de635c
df275 <df275@EVIL>
2026-01-18 18:32:59 +0500

    Реализованная страница Main. Чернышков

---
+++ Documents_Чернышков/Pages/Main.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Documents_Чернышков.Classes;
+
+namespace Documents_Чернышков.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CreatedUI();
+        }
+        public void Exit(object sender, EventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+        public void CreatedUI()
+        {
+            Parent.Children.Clear();    
+            foreach (DocumentContext document in MainWindow.init.AllDocuments)
+                Parent.Children.Add(new Elements.Item(document));
+        }
+    }
+}
