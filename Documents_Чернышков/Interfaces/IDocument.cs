edce8dbfdaa8acff23cc3b3e6a4ac16485412168
df275 <df275@EVIL>
2026-01-18 16:26:02 +0500

    Реализованные интерфейсы. Чернышков.

---
+++ Documents_Чернышков/Interfaces/IDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Чернышков.Interfaces
+{
+    internal interface IDocument
+    {
+
+        void save(bool Update =  false);
+        List<Documents.Classes.DocumentContext> AllDocuments();
+
+        void Delete();
+    }
+}
