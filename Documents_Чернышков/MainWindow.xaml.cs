a047416c8672038b5dcbdc4496058c664e08a528
df275 <df275@EVIL>
2026-01-18 18:37:38 +0500

    Реализованное окно MainWindow. Чернышков

--- Documents_Чернышков/MainWindow.xaml.cs
+++ Documents_Чернышков/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace Documents_Чернышков
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public List<DocumentContext> AllDocuments = new DocumentContext().AllDocuments();
+        public enum pages
+        {
+            main,
+            add
+        }
         public MainWindow()
         {
             InitializeComponent();
+
+            init = this;
+
+            OpenPages(pages.main);
+        }
+
+        public void OpenPages(pages _pages)
+        {
+            if (_pages == pages.main)
+                frame.Navigate(new Pages.Main());
+            else if (_pages == pages.add)
+                frame.Navigate(new Pages.Add());
         }
     }
 }
